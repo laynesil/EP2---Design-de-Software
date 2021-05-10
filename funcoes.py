de5d7319f8dc2277541f1db97c154df192520c90
layne <layne.silva.ismart@gmail.com>
2021-05-09 22:31:28 -0300

    criando função de possíveis movimentos

--- funcoes.py
+++ funcoes.py
@@ -15,3 +15,27 @@ def extrai_naipe(carta):
 def extrai_valor(carta):
     valor = carta[:-1]
     return valor
+
+def lista_movimentos_possiveis(baralho, indice):
+    if indice == 0:
+        return []
+    if indice > 0 and indice < 3:
+        if (extrai_valor(baralho[indice]) == extrai_valor(baralho[indice-1])) or (extrai_naipe(baralho[indice]) == extrai_naipe(baralho[indice-1])):
+            return [1] 
+        else:
+            return []
+    if indice >= 3:
+        mesmo_valor_1 = (extrai_valor(baralho[indice]) == extrai_valor(baralho[indice-1]))
+        mesmo_valor_3 = (extrai_valor(baralho[indice]) == extrai_valor(baralho[indice-3]))
+        mesmo_naipe_1 = (extrai_naipe(baralho[indice]) == extrai_naipe(baralho[indice-1]))
+        mesmo_naipe_3 = (extrai_naipe(baralho[indice]) == extrai_naipe(baralho[indice-3]))
+
+        if (mesmo_valor_1 or mesmo_naipe_1 ) and ( mesmo_valor_3 or mesmo_naipe_3 ):
+            return [1, 3]
+        elif mesmo_valor_1 or mesmo_naipe_1:
+            return [1]
+        elif mesmo_valor_3 or mesmo_naipe_3:
+            return [3]
+        
+        else:
+            return []
