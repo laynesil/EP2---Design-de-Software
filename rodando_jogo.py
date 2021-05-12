d2ae08d78394da3579cac1863be1e60b1e0580bd
layne <layne.silva.ismart@gmail.com>
2021-05-11 23:47:49 -0300

    melhorando usuabilidade de loopings e condicionais

--- rodando_jogo.py
+++ rodando_jogo.py
@@ -1,48 +1,88 @@
 from funcoes import *
 
+print('''
+Paciência Acordeão 
+================== 
+
+Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha. 
+
+Existem apenas dois movimentos possíveis: 
+
+1. Empilhar uma carta sobre a carta imediatamente anterior; 
+2. Empilhar uma carta sobre a terceira carta anterior. 
+
+Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida: 
+
+1. As duas cartas possuem o mesmo valor ou 
+2. As duas cartas possuem o mesmo naipe. 
+
+Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. 
+
+''')
+comecar_jogo = input('Aperte [enter] para começar a jogar: ')
+
 baralho_do_jogador = cria_baralho()
+i = 1
+for carta in baralho_do_jogador:
+    print(f"\033[34m {i}. {cor(carta)} ")
+     
+    i += 1
+
+
 e_invalida = True
 while e_invalida:
-    
-    print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.')
-    print("Existem dois movimentos possíveis:") 
-    print("1. Empilhar uma carta sobre a carta imediatamente anterior; 2. Empilhar uma carta sobre a terceira carta anterior.")
-    print("Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida:")
-    print("As duas cartas possuem o mesmo valor ou as duas cartas possuem o mesmo naipe. Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada.")
+    while len(baralho_do_jogador) > 1 and possui_movimentos_possiveis(baralho_do_jogador) == True:
+                while len(baralho_do_jogador) > 1 and possui_movimentos_possiveis(baralho_do_jogador) == True:
 
-    comecar_jogo = input('Aperte [enter] para começar a jogar: ')
-    if comecar_jogo != '':
+                    if len(baralho_do_jogador) == 1 and possui_movimentos_possiveis(baralho_do_jogador) != True:
+                        print ('Fim do jogo. Obrigado por jogar!')
+                        jogar_dnv = input('Você deseja jogar novamente ?(s/n) ')
+                        if jogar_dnv == 's':
+                            jogando = True
+                            for cada, car in enumerate(baralho_do_jogador):
+                                arg = cada + 1
+                                print(('{0}'.format(arg))+ ' ' + cor(car))
+                        else:
+                            jogando = False
 
-        print('Digite uma entrada válida!')
+                carta = int(input('Escolha o número de uma carta para jogar de {0} a {1}:'.format(1, len(baralho_do_jogador))))
 
-    else:
-        jogando = True
-        e_invalida = False
-        while jogando:
-            print("O estado atual do baralho é: ")
-            print(cartas(baralho_do_jogador))
+                while carta > len(baralho_do_jogador) or carta <= 0:
+                    print('Carta inválida!')
+                    carta = int(input('Escolha o número de uma carta para jogar de {0} a {1}:'.format(1, len(baralho_do_jogador))))
+                
+            
+                possibilidade = lista_movimentos_possiveis(baralho_do_jogador,(carta - 1))
+                
 
-            p_inicial = int(input('Escolha a posição inicial da carta que quer mexer: \nposição inicial: '))
-            possibilidades = lista_movimentos_possiveis(baralho_do_jogador, p_inicial)
-            p_final = int(input('Para onde você quer mover essa carta? \nposição final: '))
-           
-            if p_inicial - p_final == 1 or p_inicial - p_final == 3:            
-                if lista_movimentos_possiveis(baralho_do_jogador, p_inicial) == []:
-                    print('Movimento inválido. ')
-                elif (lista_movimentos_possiveis(baralho_do_jogador, p_inicial) == [1] and p_inicial - p_final == 1) or (lista_movimentos_possiveis(baralho_do_jogador, p_inicial) == [3] and p_inicial - p_final == 3) or (lista_movimentos_possiveis(baralho_do_jogador, p_inicial) == [1, 3] and (p_inicial - p_final == 1) or (p_inicial - p_final ==3)):         
-                    baralho_do_jogador = empilha(baralho_do_jogador, p_inicial, p_final)
-                    print('Está pronto para próxima rodada? Vamos lá! ')
+                while possibilidade == []:
+                    vazia = int(input('Digite uma entrada válida!'))
+                    iv = vazia - 1
+                    possibilidade = lista_movimentos_possiveis(baralho_do_jogador,iv)
+                
+                if possibilidade == [1] :
+                    for cada, car in enumerate(empilha(baralho_do_jogador, (carta - 1) , (carta - 1) - 1)):
+                        arg = cada + 1
+                        print(('{0}'.format(arg))+ ' ' + cor(car))
+                    
+                
+                if possibilidade == [3]:
+                    for cada, car in enumerate(empilha(baralho_do_jogador, (carta - 1) , (carta - 1) - 3)):
+                        arg = cada + 1
+                        print(('{0}'.format(arg))+ ' ' + cor(car))
+                    
+                
+                if possibilidade == [1,3]:
+                    mov = True
+                    while mov:
+                        empilha1 = int(input('Escolha a carta que deseja empilhar ({0} ou {1}):'.format((carta - 1), (carta - 1) - 2)))
+                        ie = empilha1 - 1
 
-                else:
-                    print('Movimento inválido. ')
-                if possui_movimentos_possiveis(baralho_do_jogador) == False:
-                    if len(baralho_do_jogador) > 1:
-                        print('Você não possui mais movimentos possíveis... Você perdeu =( ')
-                        jogando == False
-                        
-                    if len(baralho_do_jogador) == 1:
-                        print('Parabéns!!!!!!!! Você ganhou o jogo!!!!!!!')
-                        jogando == False
-                        
-            else:
-                print('Movimento inválido. ')
+                        if ie == (carta - 1) - 3 or ie == (carta - 1) - 1:
+                            for cada, car in enumerate(empilha(baralho_do_jogador, (carta - 1) , ie)):
+                                arg = cada + 1
+                                print(('{0}'.format(arg))+ ' ' + cor(car))
+                            mov = False
+                        else:
+                            print('Digite uma entrada válida!')
+                            mov = True
